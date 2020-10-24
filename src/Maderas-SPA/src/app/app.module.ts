61eed628841d8ef74554eed4211d5d40cd01ed00
Daniel Sprohar <daniel.sprohar@outlook.com>
2020-10-24 08:26:47 -0500

    Added the primary components to the auth module

--- src/Maderas-SPA/src/app/app.module.ts
+++ src/Maderas-SPA/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { AuthModule } from './auth/auth.module';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
