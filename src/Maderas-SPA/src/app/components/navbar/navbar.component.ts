68244ebc2a69a74e3c2a5df28f1cc6af465fa5f3
Daniel Sprohar <daniel.sprohar@outlook.com>
2020-10-24 08:18:10 -0500

    Added a navbar component

---
+++ src/Maderas-SPA/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  toggleMenu(): void {
+    const navbarMenu = document.getElementById('navbarMenu');
+    navbarMenu.classList.toggle('is-active');
+
+    const navbarBurger = document.getElementById('navbarBurger');
+    navbarBurger.classList.toggle('is-active');
+  }
+
+}
