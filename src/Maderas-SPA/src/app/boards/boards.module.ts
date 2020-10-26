e26ff2cfb4c64e2c847d233ec4dafa523827bfa7
Daniel Sprohar <daniel.sprohar@outlook.com>
2020-10-26 11:21:49 -0500

    Created the Lists module

--- src/Maderas-SPA/src/app/boards/boards.module.ts
+++ src/Maderas-SPA/src/app/boards/boards.module.ts
@@ -6,6 +6,7 @@ import { BoardsComponent } from './boards.component';
 import { SharedModule } from '../shared/shared.module';
 import { CreateBoardComponent } from './create-board/create-board.component';
 import { BoardShellComponent } from './board-shell/board-shell.component';
+import { ListsModule } from '../lists/lists.module';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { BoardShellComponent } from './board-shell/board-shell.component';
   imports: [
     CommonModule,
     BoardsRoutingModule,
-    SharedModule
+    SharedModule,
+    ListsModule
   ]
 })
 export class BoardsModule { }
