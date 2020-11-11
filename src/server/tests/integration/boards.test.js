01441ad1b7c954317a0126576c2af21698f0af3f
Daniel Sprohar <daniel.sprohar@outlook.com>
2020-11-11 17:22:42 -0600

    Finished testing all the Boards' endpoints

--- src/server/tests/integration/boards.test.js
+++ src/server/tests/integration/boards.test.js
@@ -1,5 +1,6 @@
 const request = require('supertest')
 const { Board } = require('../../models/board')
+const { Template } = require('../../models/template')
 const mongoose = require('mongoose')
 const httpStatusCodes = require('../../constants/http-status-codes')
 
@@ -33,12 +34,26 @@ async function clearDb() {
 }
 
 // ===========================================================================
+
+async function createInvalidObjectIdRequest() {
+  const res = await request(server).get('/api/boards/1')
+  expect(res.status).toBe(httpStatusCodes.unprocessableEntity)
+}
+
+// ===========================================================================
+
+async function createBoardNotFoundRequest() {
+  const id = new mongoose.Types.ObjectId().toHexString()
+  const res = await request(server).get(`/api/boards/${id}`)
+  expect(res.status).toBe(httpStatusCodes.notFound)
+}
+
+// ===========================================================================
 // Top-level test suite
 // ===========================================================================
 describe('/api/boards', () => {
   beforeEach(async () => {
     server = require('../../app')
-    await initializeDb()
   })
 
   afterEach(async () => {
@@ -53,25 +68,26 @@ describe('/api/boards', () => {
   // =========================================================================
   describe('GET /', () => {
     it('should return a paginated list of boards', async () => {
+      await initializeDb()
       const res = await request(server).get('/api/boards')
+
       expect(res.status).toBe(httpStatusCodes.ok)
-      expect(res.body['data'].length).toBe(3)
+      expect(res.body['data'].length).toBeGreaterThan(1)
     })
   })
 
   // =========================================================================
 
   describe('GET /:id', () => {
-    it('should return HTTP status 422 for an invalid object id', async () => {
-      const res = await request(server).get('/api/boards/1')
-      expect(res.status).toBe(httpStatusCodes.unprocessableEntity)
-    })
+    it(
+      'should return HTTP status 422 for an invalid object id',
+      createInvalidObjectIdRequest
+    )
 
-    it('should return HTTP status 404 for a board that does not exist', async () => {
-      const id = new mongoose.Types.ObjectId().toHexString()
-      const res = await request(server).get(`/api/boards/${id}`)
-      expect(res.status).toBe(httpStatusCodes.notFound)
-    })
+    it(
+      'should return HTTP status 404 for a board that does not exist',
+      createBoardNotFoundRequest
+    )
 
     it('should return HTTP status 200 for a board that exists', async () => {
       const board = new Board({ title: 'test', user: userId })
@@ -83,4 +99,136 @@ describe('/api/boards', () => {
   })
 
   // =========================================================================
+
+  describe('POST /', () => {
+    it('should return HTTP status 422 for an invalid request body', async () => {
+      const res = await request(server).post('/api/boards').send({})
+      expect(res.status).toBe(httpStatusCodes.unprocessableEntity)
+    })
+
+    it('should return HTTP status 201 upon successful creation of a new', async () => {
+      const payload = {
+        title: 'test',
+        user: userId
+      }
+
+      const res = await request(server).post('/api/boards').send(payload)
+      expect(res.status).toBe(httpStatusCodes.created)
+      expect(res.body).toBeTruthy()
+      expect(res.body).toMatchObject(payload)
+    })
+  })
+
+  // =========================================================================
+
+  describe('PUT /:id', () => {
+    it(
+      'should return HTTP status 422 for an invalid object id',
+      createInvalidObjectIdRequest
+    )
+
+    it(
+      'should return HTTP status 404 for a board that does not exist',
+      createBoardNotFoundRequest
+    )
+
+    it('should return HTTP status 422 for an invalid request body', async () => {
+      const board = new Board({ title: 'test', user: userId })
+      await board.save()
+      const url = `/api/boards/${board._id}`
+      const res = await request(server).put(url).send({})
+      expect(res.status).toBe(httpStatusCodes.unprocessableEntity)
+    })
+
+    it('should update a board and return said board', async () => {
+      const board = new Board({ title: 'test', user: userId })
+      await board.save()
+
+      const url = `/api/boards/${board._id}`
+      const payload = {
+        title: 'test (edit)',
+        user: userId
+      }
+
+      const res = await request(server).put(url).send(payload)
+      expect(res.status).toBe(httpStatusCodes.ok)
+      expect(res.body).toBeTruthy()
+      expect(res.body).toMatchObject(payload)
+    })
+  })
+
+  // =========================================================================
+
+  describe('DELETE /:id', () => {
+    it(
+      'should return HTTP status 422 for an invalid object id',
+      createInvalidObjectIdRequest
+    )
+
+    it(
+      'should return HTTP status 404 for a board that does not exist',
+      createBoardNotFoundRequest
+    )
+
+    it('should delete a board and all of its related documents', async () => {
+      const board = new Board({ title: 'test', user: userId })
+      await board.save()
+
+      const url = `/api/boards/${board._id}`
+      const res = await request(server).delete(url)
+      expect(res.status).toBe(httpStatusCodes.noContent)
+    })
+  })
+
+  // =========================================================================
+
+  describe('POST /create-from/template/:id', () => {
+    it('should return HTTP status 422 for an invalid request body', async () => {
+      const id = new mongoose.Types.ObjectId().toHexString()
+      const url = `/api/boards/create-from/template/${id}`
+      const res = await request(server).post(url).send({})
+      expect(res.status).toBe(httpStatusCodes.unprocessableEntity)
+    })
+
+    it('should return HTTP status 422 for an invalid object id', async () => {
+      const url = '/api/boards/create-from/template/1'
+      const payload = {
+        title: 'test',
+        user: userId
+      }
+
+      const res = await request(server).post(url).send(payload)
+      expect(res.status).toBe(httpStatusCodes.unprocessableEntity)
+    })
+
+    it('should return HTTP status 404 for a board that does not exist', async () => {})
+
+    it('should create a new board from an existing template', async () => {
+      const template = new Template({
+        name: 'Kanban',
+        lists: [
+          { title: 'Backlog' },
+          { title: 'Design' },
+          { title: 'To-Do' },
+          { title: 'In progress' },
+          { title: 'Code Review' },
+          { title: 'Testing' },
+          { title: 'Done' }
+        ]
+      })
+
+      await template.save()
+
+      const url = `/api/boards/create-from/template/${template._id}`
+      const payload = {
+        title: 'test',
+        user: userId
+      }
+
+      const res = await request(server).post(url).send(payload)
+      expect(res.status).toBe(httpStatusCodes.created)
+      expect(res.body).toBeTruthy()
+      expect(res.body).toMatchObject(payload)
+    })
+  })
 })
